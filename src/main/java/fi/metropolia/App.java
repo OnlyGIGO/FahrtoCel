88ff1d7806d765f52094f0717d1f13fb5c46eea4
vilppu <blaanblaa123@gmail.com>
2024-03-03 13:06:45 +0200

    test commit

--- src/main/java/fi/metropolia/App.java
+++ src/main/java/fi/metropolia/App.java
@@ -5,5 +5,6 @@ public class App {
         System.out.println("32 Fahrenheit is "+FahrToCel.convertFahrToCel(32)+" Celsius");
         System.out.println("212 Kelvin is "+FahrToCel.kelvintoCel(212)+" Celsius");
         System.out.println("300.1 Kelvin is "+FahrToCel.kelvinToFahr(300.1f)+" Fahrenheit");
+
     }
 }
